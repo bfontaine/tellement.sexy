0cdececa31cb6013b35e39fbe3d599cd57fd6a22
Baptiste Fontaine <b@ptistefontaine.fr>
2017-02-05 14:15:26 +0100

    Add a couple aliases

--- app.py
+++ app.py
@@ -5,27 +5,45 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+BASE = "tellement.sexy"
+VERBS = {
+    "est": "est",
+    "sont": "sont",
+    "is": "is",
+    "are": "are",
+    "cest": "c'est",
+    "ete": "été",
+    "etre": "être",
+    "etait": "était",
+    "etais": "étais",
+}
+ALIASES = {
+    "css": "CSS",
+    "html": "HTML",
+    "javascript": "JavaScript",
+    "lisp": "LISP",
+    "php": "PHP",
+}
+
+
+def pretty_word(word):
+    if word in ALIASES:
+        return ALIASES[word]
+
+    return word.capitalize()
+
 @app.route("/")
 def root():
-    base = "tellement.sexy"
-    verbs = {
-        "est": "est",
-        "sont": "sont",
-        "is": "is",
-        "are": "are",
-        "cest": "c'est",
-    }
-
     host = urlparse(request.base_url).netloc.split(":", 1)[0]
 
-    for verb, text in verbs.items():
-        idx = host.rfind(".%s.%s" % (verb, base))
+    for verb, text in VERBS.items():
+        idx = host.rfind(".%s.%s" % (verb, BASE))
         if idx < 0:
             continue
 
         parts = host[:idx].split(".")
 
-        name = " ".join([p.capitalize() for p in parts])
+        name = " ".join([pretty_word(w) for w in parts])
         return "%s %s tellement sexy ;)" % (name, text)
 
     abort(404)
